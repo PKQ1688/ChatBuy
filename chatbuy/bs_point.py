63b812ca0e170dbd862154f321d7b0ec116cc1f3
Adolf Zhu <adolf.zhu@flsdex.com>
2024-12-04 14:03:36 +0800

    Enhance cryptocurrency analysis by adding Bollinger Bands and improving MACD calculations; update project dependencies and ensure data directory exists for CSV exports.

--- chatbuy/bs_point.py
+++ chatbuy/bs_point.py
@@ -1,26 +1,22 @@
 from pathlib import Path
 from phi.agent import Agent
-from phi.tools.pandas import PandasTools
-from phi.model.azure import AzureOpenAIChat
 import pandas as pd
-
+from chatbuy.llm_models import llm_model_0806 as llm_model
 
 csv_path = Path("data/BTC_USDT_1d_with_indicators.csv")
-dataframes = {"btc": pd.read_csv(csv_path)}
+dataframes = pd.read_csv(csv_path)
 
 agent = Agent(
-    model=AzureOpenAIChat(id="gpt4o"),
-    tools=[PandasTools()],
+    model=llm_model,
     markdown=True,
     show_tool_calls=True,
-    # instructions=[
-    #     # "First always get the list of files",
-    #     # "Then check the columns in the file",
-    #     # "Then run the query to answer the question",
-    # ],
+    instructions=[
+        "data:",
+        dataframes[-60:].to_markdown(index=False),
+    ],
+    debug_mode=True,
 )
+
 agent.print_response(
-    """找到其中列名为histogram指标由负变正和由正变负的临界点从 `csv_data`, 
-    use: {"dataframe_name": "csv_data", "operation": "head", "operation_parameters": {"n": 5}}""", 
-    stream=True
+    """找到其中列名为histogram指标由负变正和由正变负的所有临界点""", stream=False
 )
