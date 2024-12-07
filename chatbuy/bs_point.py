c1198fe826d78b83943de0c647062503c788b91a
adolf <adolf1321794021@gmail.com>
2024-12-08 00:46:35 +0800

    Refactor LLM model integration by moving model imports to a new scripts directory, update dependencies, and implement synchronous agent execution

--- chatbuy/bs_point.py
+++ chatbuy/bs_point.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from chatbuy.llm_models import model_0806 as model
+from scripts.llm_models import model_4o_mini as model
 from pathlib import Path
 
 from pydantic_ai import Agent
@@ -11,6 +11,7 @@ agent = Agent(
     model=model,
     deps_type=str,
     result_type=str,
+    system_prompt="你是一名优秀的交易员,现在你需要找到根据我给你提供的数据来找到关键位置.",
 )
 
 
@@ -21,11 +22,13 @@ async def system_prompt() -> str:
 
 
 if __name__ == "__main__":
-    import asyncio
+    # import asyncio
+    from rich import print
 
-    result = asyncio.run(
-        agent.run("找到其中列名为histogram指标由负变正和由正变负的所有临界点")
-    )
-
+    # result = asyncio.run(
+    #     agent.run("找到其中列名为histogram指标由负变正和由正变负的所有临界点")
+    # )
+    result = agent.run_sync("找到其中列名为histogram指标由负变正和由正变负的所有临界点")
+    print(result.all_messages())
     print(result.data)
     # print(result.cost)
