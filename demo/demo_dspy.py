a8f584fd5199e8136a074bb39facbc5b36765a07
PKQ1688 <adolf1321794021@gmail.com>
2024-12-14 17:14:12 +0800

    Enhance demo_dspy.py: add pretty printing for trainset and comment out unused ReAct code for clarity

--- demo/demo_dspy.py
+++ demo/demo_dspy.py
@@ -1,7 +1,7 @@
 import dspy
 from scripts.dspy_lm import lm_4o_mini as lm
 from dspy.datasets import HotPotQA
-
+from rich.pretty import pprint
 
 dspy.configure(lm=lm)
 
@@ -17,9 +17,18 @@ def search(query: str) -> list[str]:
 trainset = [
     x.with_inputs("question") for x in HotPotQA(train_seed=2024, train_size=500).train
 ]
-print(trainset[0])
 
-react = dspy.ReAct("question -> answer", tools=[search])
+pprint(trainset)
+
+# react = dspy.ReAct("question -> answer", tools=[search])
 
 # tp = dspy.MIPROv2(metric=dspy.evaluate.answer_exact_match, auto="light", num_threads=24)
 # optimized_react = tp.compile(react, trainset=trainset)
+
+# optimized_react.save("demo/optimized_react.json")
+# react.load("demo/optimized_react.json")
+# pred = react(question=trainset[0].question)
+
+# print(pred)
+# 
+# lm.inspect_history()
