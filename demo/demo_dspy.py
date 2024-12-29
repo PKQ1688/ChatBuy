f2996287119a264931bd87f682bbed1bc59d954c
PKQ1688 <adolf1321794021@gmail.com>
2024-12-29 15:56:11 +0800

    Add Azure OpenAI model integration and clean up codebase

--- demo/demo_dspy.py
+++ demo/demo_dspy.py
@@ -1,34 +1,35 @@
 import dspy
-from scripts.dspy_lm import lm_4o_mini as lm
 from dspy.datasets import HotPotQA
-from rich.pretty import pprint
 from dspy.evaluate import Evaluate
+from rich.pretty import pprint
+
+from chatbuy.base_model.dspy_lm import lm_4o_mini as lm
 
 dspy.configure(lm=lm)
 
 
 def search(query: str) -> list[str]:
     """Retrieves abstracts from Wikipedia."""
-    results = dspy.ColBERTv2(url="http://20.102.90.50:2017/wiki17_abstracts")(
+    results = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')(
         query, k=3
     )
-    return [x["text"] for x in results]
+    return [x['text'] for x in results]
 
 
 trainset = [
-    x.with_inputs("question")
+    x.with_inputs('question')
     for x in HotPotQA(train_seed=2024, train_size=50, dev_size=500).train
 ]
 
 devset = [
-    x.with_inputs("question")
+    x.with_inputs('question')
     for x in HotPotQA(train_seed=2023, train_size=50, dev_size=500).dev
 ]
 
 # breakpoint()
 
 pprint(trainset)
-react = dspy.ReAct("question -> answer", tools=[search])
+react = dspy.ReAct('question -> answer', tools=[search])
 # react.load("demo/optimized_react.json")
 
 
