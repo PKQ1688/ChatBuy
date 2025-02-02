f745b16f30261e45014db74681b143c7a56194c2
adofe <adofezhu@gmail.com>
2025-02-03 00:35:12 +0800

    Add DeepSeek client integration and update dependencies

--- chatbuy/base_model/pydantic_lm.py
+++ chatbuy/base_model/pydantic_lm.py
@@ -2,7 +2,7 @@ import os
 
 # import httpx
 from dotenv import load_dotenv
-from openai import AsyncAzureOpenAI
+from openai import AsyncAzureOpenAI, OpenAI
 from pydantic_ai.models.openai import OpenAIModel
 
 load_dotenv(override=True)
@@ -14,17 +14,24 @@ load_dotenv(override=True)
 #     'https://': os.getenv('PROXY_HTTPS', 'http://mwg-hkidc.kucoin.net:9090'),
 # }
 
-client = AsyncAzureOpenAI(
+
+deepseek_client = OpenAI(
+    api_key=os.environ["DEEPSEEK_API_KEY"], base_url="https://api.deepseek.com"
+)
+
+gpt_client = AsyncAzureOpenAI(
     azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
     api_version=os.environ["AZURE_OPENAI_API_VERSION"],
     api_key=os.environ["AZURE_OPENAI_API_KEY"],
     # http_client=httpx.AsyncClient(proxies=proxies, verify=False) if use_proxy else None,
 )
 
-model_4o = OpenAIModel("gpt4o", openai_client=client)
-model_0806 = OpenAIModel("gpt-4o-0806", openai_client=client)
-model_1120 = OpenAIModel("gpt-4o-1120", openai_client=client)
-model_mini = OpenAIModel("gpt-4o-mini", openai_client=client)
+model_reason = OpenAIModel("deepseek-reasoner", openai_client=deepseek_client)
+
+model_4o = OpenAIModel("gpt4o", openai_client=gpt_client)
+model_0806 = OpenAIModel("gpt-4o-0806", openai_client=gpt_client)
+model_1120 = OpenAIModel("gpt-4o-1120", openai_client=gpt_client)
+model_mini = OpenAIModel("gpt-4o-mini", openai_client=gpt_client)
 
 
 if __name__ == "__main__":
