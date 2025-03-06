ec14e9531ed4c4e3308fb686edf3378b04efd0e6
Adolf Zhu <adolf.zhu@flsdex.com>
2025-03-06 14:49:07 +0800

    Refactor pydantic_lm.py and smol_lm.py to streamline model initialization and update environment variable handling for Azure API

--- chatbuy/base_model/pydantic_lm.py
+++ chatbuy/base_model/pydantic_lm.py
@@ -1,9 +1,6 @@
 import os
 
-# import httpx
 from dotenv import load_dotenv
-
-# from openai import AsyncAzureOpenAI, OpenAI
 from openai import AsyncAzureOpenAI
 from pydantic_ai.models.groq import GroqModel
 from pydantic_ai.models.openai import OpenAIModel
@@ -11,38 +8,27 @@ from pydantic_ai.providers.openai import OpenAIProvider
 
 load_dotenv(override=True)
 
-# use_proxy = True
 
-# proxies = {
-#     'http://': os.getenv('PROXY_HTTP', 'http://mwg-hkidc.kucoin.net:9090'),
-#     'https://': os.getenv('PROXY_HTTPS', 'http://mwg-hkidc.kucoin.net:9090'),
-# }
-
-
-deepseek_client = OpenAIProvider(
+deepseek_provider = OpenAIProvider(
     api_key=os.environ["DEEPSEEK_API_KEY"], base_url="https://api.deepseek.com"
 )
 
 gpt_client = AsyncAzureOpenAI(
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-    api_version=os.environ["AZURE_OPENAI_API_VERSION"],
-    api_key=os.environ["AZURE_OPENAI_API_KEY"],
-    # http_client=httpx.AsyncClient(proxies=proxies, verify=False) if use_proxy else None,
+    azure_endpoint=os.environ["AZURE_API_BASE"],
+    api_version=os.environ["AZURE_API_VERSION"],
+    api_key=os.environ["AZURE_API_KEY"],
 )
 
-model_reason = OpenAIModel("deepseek-reasoner", provider=deepseek_client)
+gpt_provider = OpenAIProvider(openai_client=gpt_client)
 
-model_4o = OpenAIModel("gpt4o", provider=OpenAIProvider(openai_client=gpt_client))
-model_0806 = OpenAIModel(
-    "gpt-4o-0806", provider=OpenAIProvider(openai_client=gpt_client)
-)
-model_1120 = OpenAIModel(
-    "gpt-4o-1120", provider=OpenAIProvider(openai_client=gpt_client)
-)
-model_mini = OpenAIModel(
-    "gpt-4o-mini", provider=OpenAIProvider(openai_client=gpt_client)
-)
+model_reason = OpenAIModel("deepseek-reasoner", provider=deepseek_provider)
 
+model_4o = OpenAIModel("gpt4o", provider=gpt_provider)
+model_0806 = OpenAIModel("gpt-4o-0806", provider=gpt_provider)
+model_1120 = OpenAIModel("gpt-4o-1120", provider=gpt_provider)
+model_mini = OpenAIModel("gpt-4o-mini", provider=gpt_provider)
+
+model_qwen32 = GroqModel("qwen-2.5-32b", api_key=os.getenv("GROQ_API_KEY"))
 model_qwq32 = GroqModel("qwen-qwq-32b", api_key=os.getenv("GROQ_API_KEY"))
 
 if __name__ == "__main__":
