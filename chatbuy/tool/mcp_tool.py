fb1a8e71cdb808e5692f3c3ddd74c8eff2580b44
Adolf Zhu <adolf.zhu@flsdex.com>
2025-03-09 15:58:27 +0800

    Refactor MCP tool integration to conditionally use CodeAgent and update pydantic-ai dependency

--- chatbuy/tool/mcp_tool.py
+++ chatbuy/tool/mcp_tool.py
@@ -9,7 +9,7 @@ from smolagents import CodeAgent, ToolCallingAgent, ToolCollection
 #     args=["--quiet", "pubmedmcp@0.1.3"],
 #     env={"UV_PYTHON": "3.12", **os.environ},
 # )
-
+use_code = False
 
 server_parameters = StdioServerParameters(
     command="uvx",  # Executable
@@ -34,10 +34,13 @@ server_parameters = StdioServerParameters(
 
 
 with ToolCollection.from_mcp(server_parameters) as tool_collection:
-    # agent = CodeAgent(tools=[*tool_collection.tools], add_base_tools=False, model=model)
-    agent = ToolCallingAgent(
-        tools=[*tool_collection.tools], model=model, add_base_tools=False
-    )
+
+    if use_code:
+        agent = CodeAgent(tools=[*tool_collection.tools], add_base_tools=False, model=model)
+    else:
+        agent = ToolCallingAgent(
+            tools=[*tool_collection.tools], model=model, add_base_tools=False
+        )
     agent.run(
         "帮我使用中文总结一下这个 https://huggingface.co/blog/smolagents-phoenix 网页的内容"
     )
