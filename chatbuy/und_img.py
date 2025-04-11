7da0ccafc3293542e8230303e4b8eeac8467db68
Adolf Zhu <adolf.zhu@flsdex.com>
2025-04-11 11:32:10 +0800

    Update image path and refactor agent initialization for clarity

--- chatbuy/und_img.py
+++ chatbuy/und_img.py
@@ -13,7 +13,7 @@ provider = OpenAIProvider(
 )
 model = OpenAIModel("openrouter/quasar-alpha", provider=provider)
 
-image_path = "data/BTC_indicators_20250407_211726.png"  # 替换为你的本地图片路径
+image_path = "data/BTC_indicators_20250408_165321.png"  # 替换为你的本地图片路径
 with open(image_path, "rb") as image_file:
     image_content = image_file.read()
 
@@ -26,14 +26,19 @@ class TradeAdvice(BaseModel):
     reason : str
         The reason for the recommended action.
     """
+
     action: str  # Possible values: "hold", "sell", "buy"
     reason: str
 
 
-agent = Agent(model=model, result_type=TradeAdvice)
+agent = Agent(
+    model=model,
+    result_type=TradeAdvice,
+    system_prompt="根据我提供给的策略，给出交易判断",
+)
 result = agent.run_sync(
     [
-        "帮我分析一下这个图表的走势",
+        "策略如下:如果到布林道下轨就买入，上轨到达时卖出，剩下的时候就观望",
         BinaryContent(data=image_content, media_type="image/png"),
     ]
 )
